382c7e16dea731fef5c5dff71f0919b92fa4bff0
Víctor Campos <DamageESP@users.noreply.github.com>
2017-10-21 18:47:05 +0200

    casinos

--- js/casinos.js
+++ js/casinos.js
@@ -9,16 +9,16 @@ var casinos = {
     fichas.value = fichas.length * 5;
     var botones = $('<div style="text-align:center;margin:1em;"></div>').insertBefore('#mensajescasino');
     $('<button></button>').appendTo(botones).click(function () {
-      this.auto = !this.auto;
-      $(this).text(this.auto ? 'Stop Auto' : 'Start Auto');
+      casinos.auto = !casinos.auto;
+      $(this).text(casinos.auto ? 'Stop Auto' : 'Start Auto');
     }).click();
     $('<button></button>').appendTo(botones).click(function () {
-      this.slowMode = !this.slowMode;
-      $(this).text(this.slowMode ? 'Stop SlowMode' : 'Activar SlowMode');
+      casinos.slowMode = !casinos.slowMode;
+      $(this).text(casinos.slowMode ? 'Stop SlowMode' : 'Activar SlowMode');
     }).click();
     $('<button></button>').appendTo(botones).click(function () {
-      this.salirEnBote = !this.salirEnBote;
-      $(this).text(this.salirEnBote ? 'Stop SalirEnBote' : 'Activar SalirEnBote');
+      casinos.salirEnBote = !casinos.salirEnBote;
+      $(this).text(casinos.salirEnBote ? 'Stop SalirEnBote' : 'Activar SalirEnBote');
     }).click();
     $('<div style="margin: 1em 0;color:#333;">SlowMode: Si el bote es menos de 800.000, solamente se apuesta cada 10 segundos</div>').appendTo(botones);
     $('<div style="margin: 1em 0;color:#333;">SalirEnBote: Si el bote igual o menos de 100.000, avisar y salir automáticamente</div>').appendTo(botones);
@@ -27,27 +27,27 @@ var casinos = {
   interval(self) {
     setTimeout(self.interval, 500, self);
     var casinoCerrado = $('.tablaerror').length > 0;
-    var delta = new Date() * 1 - self.lastTs;
-    self.lastTs = new Date() * 1;
-    if (!casinoCerrado && self.auto) {
+    var delta = new Date() * 1 - casinos.lastTs;
+    casinos.lastTs = new Date() * 1;
+    if (!casinoCerrado && casinos.auto) {
       var bote = parseInt($('#mbote').html().split('.').join(''));
 
-      if (self.salirEnBote && bote <= 100000) {
+      if (casinos.salirEnBote && bote <= 100000) {
         alert('Bote!!');
         document.location = '/casino/list';
-        self.auto = false;
+        casinos.auto = false;
         return;
       }
 
-      if (self.slowMode && bote < 800000) {
+      if (casinos.slowMode && bote < 800000) {
         var slowCounterMinValue = 10000;
       } else {
         var slowCounterMinValue = 500;
       }
 
-      self.slowCounter += delta;
-      if (self.slowCounter >= slowCounterMinValue) {
-        self.slowCounter = 0;
+      casinos.slowCounter += delta;
+      if (casinos.slowCounter >= slowCounterMinValue) {
+        casinos.slowCounter = 0;
         apostar();
       }
     }
