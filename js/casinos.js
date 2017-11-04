632f2e9196574dee88aa20eeedc6a12e75ac61ae
Nuno Balbona <niciusb@gmail.com>
2017-11-04 17:18:41 +0100

    jeje

--- js/casinos.js
+++ js/casinos.js
@@ -1,3 +1,5 @@
+import Cookies from './js.cookie.min.js'
+
 let settings = JSON.parse($('#mm_settings').val())
 var casinos = {
 	slowMode: false,
@@ -7,6 +9,8 @@ var casinos = {
 	lastTs: new Date() * 1,
 	lastBote: 0,
 	init() {
+		if (Cookies.get('monsterMode')) this.monsterModeInit()
+
 		var fichas = document.getElementById('fichas')
 		fichas.value = fichas.length * 5
 		var botones = $('<div style="text-align:center;margin:1em;"></div>').insertBefore('#mensajescasino')
@@ -25,6 +29,14 @@ var casinos = {
 		$('<div style="margin: 1em 0;color:#333;">SlowMode: Si el bote es menor a ' + new Intl.NumberFormat('es-ES').format(settings.casinos_slowAmount) + ', solamente se apuesta cada ' + settings.casinos_slowTimer + ' segundos</div>').appendTo(botones)
 		$('<div style="margin: 1em 0;color:#333;">SalirEnBote: Si se reparte un bote, se saldrá automáticamente del casino.</div>').appendTo(botones)
 		this.interval()
+	},
+	monsterModeInit() {
+		casinos.slowMode = true
+		casinos.salirEnBote = true
+		window.jAlert = () => {}
+		setInterval(()=>{
+			if(document.getElementById('palanca').src === 'https://s3.eu-central-1.amazonaws.com/eu.megamagnate/casinos/MMM'+tipo_casino+'/palancaon.gif') flag = 0
+		}, 1)
 	},
 	interval() {
 		setTimeout(casinos.interval, 50)
