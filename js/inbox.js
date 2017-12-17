89c0cb5adecbd7ad8946ddea0b1ec967471e7e7f
Víctor Campos <DamageESP@users.noreply.github.com>
2017-12-17 22:46:54 +0100

    HiLo solo continua si vale la pena

--- js/inbox.js
+++ js/inbox.js
@@ -4,7 +4,7 @@ import messageDB from './messageDB.js'
 module.exports = {
   init() {
     espionajes.init()
-    $(".reporteMision").each((i, el) => {
+    $('.reporteMision').each((i, el) => {
       const messageId = $(el).parents('.tablaContenido2').attr('id').replace('id_mensaje_', '')
       messageDB.shareMessage(messageId, el.textContent)
     })
--- js/main.js
+++ js/main.js
@@ -13,35 +13,35 @@ let mode = settings.advanced_devMode ? 'dev' : 'production'
 console.log('Mega Megamagnate '+ settings.version +' (' + mode + ' mode) is loaded')
 
 function switchLocation(loc) {
-	switch (loc[0]) {
-	case 'buildings':
-		buildings.init()
-		break
-	case 'casino':
-		if (loc[1] === 'play') casinos.init()
-		else if (loc[1] === 'hilo') HiLo.init()
-		break
-		case 'messages':
-			if (loc[1] === 'inbox') inbox.init()
-			break
-		case 'missions':
-			misiones.init()
-			break
-	}
+  switch (loc[0]) {
+  case 'buildings':
+    buildings.init()
+    break
+  case 'casino':
+    if (loc[1] === 'play') casinos.init()
+    else if (loc[1] === 'hilo') HiLo.init()
+    break
+  case 'messages':
+    if (loc[1] === 'inbox') inbox.init()
+    break
+  case 'missions':
+    misiones.init()
+    break
+  }
 }
 
 window.formatNumber = function (numero) {
-	var num = new window.NumberFormat()
-	num.setInputDecimal('.')
-	num.setNumber(numero)
-	num.setPlaces('0', false)
-	num.setCurrencyValue('')
-	num.setCurrency(true)
-	num.setCurrencyPosition(num.RIGHT_OUTSIDE)
-	num.setNegativeFormat(num.LEFT_DASH)
-	num.setNegativeRed(true)
-	num.setSeparators(true, '.', '.')
-	return num.toFormatted()
+  var num = new window.NumberFormat()
+  num.setInputDecimal('.')
+  num.setNumber(numero)
+  num.setPlaces('0', false)
+  num.setCurrencyValue('')
+  num.setCurrency(true)
+  num.setCurrencyPosition(num.RIGHT_OUTSIDE)
+  num.setNegativeFormat(num.LEFT_DASH)
+  num.setNegativeRed(true)
+  num.setSeparators(true, '.', '.')
+  return num.toFormatted()
 }
 
 $('.footer').hide()
@@ -49,30 +49,30 @@ $('.footer').hide()
 if(!settings.general_playButton) $('.trozoLogin a[href="https://play.google.com/store/apps/details?id=net.megamagnate"]').hide()
 
 if (settings.general_moneyTimer) {
-	$('.cajaDinero').css('font-family', 'monospace').prepend('<span id="tiempoHastaLlenado" style="font-size: 0.225em;color:#aaa;"></span>')
-	$('#cajetoDinero').css('font-size', '0.85em').html(window.formatNumber(window.dinero))
-	if ($('#cajetoDinero').attr('data')) {
-		setInterval(() => {
-			var toHHMMSS = secs => {
-				var sec_num = parseInt(secs, 10)
-				var hours = Math.floor(sec_num / 3600) // Como la unidad máx de medida es horas, que puedan superar las 24
-				var minutes = Math.floor(sec_num / 60) % 60
-				var seconds = sec_num % 60
-				return [hours, minutes, seconds]
-					.map(v => v < 10 ? '0' + v : v)
-					.filter((v, i) => v !== '00' || i > 0)
-					.join(':')
-			}
-			var limite_caja = parseInt($('#cajetoDinero').attr('data').split(':')[1].split('.').join(''))
-			var al_segundo = parseFloat($('#alsegundo').html())
-			var segundos_que_quedan = (limite_caja - window.dinero) / al_segundo
-			if (segundos_que_quedan > 0) {
-				$('#tiempoHastaLlenado').html(toHHMMSS(segundos_que_quedan) + ' hasta llenado')
-			} else {
-				$('#tiempoHastaLlenado').html('Lleno')
-			}
-		}, 500)
-	}
+  $('.cajaDinero').css('font-family', 'monospace').prepend('<span id="tiempoHastaLlenado" style="font-size: 0.225em;color:#aaa;"></span>')
+  $('#cajetoDinero').css('font-size', '0.85em').html(window.formatNumber(window.dinero))
+  if ($('#cajetoDinero').attr('data')) {
+    setInterval(() => {
+      var toHHMMSS = secs => {
+        var sec_num = parseInt(secs, 10)
+        var hours = Math.floor(sec_num / 3600) // Como la unidad máx de medida es horas, que puedan superar las 24
+        var minutes = Math.floor(sec_num / 60) % 60
+        var seconds = sec_num % 60
+        return [hours, minutes, seconds]
+          .map(v => v < 10 ? '0' + v : v)
+          .filter((v, i) => v !== '00' || i > 0)
+          .join(':')
+      }
+      var limite_caja = parseInt($('#cajetoDinero').attr('data').split(':')[1].split('.').join(''))
+      var al_segundo = parseFloat($('#alsegundo').html())
+      var segundos_que_quedan = (limite_caja - window.dinero) / al_segundo
+      if (segundos_que_quedan > 0) {
+        $('#tiempoHastaLlenado').html(toHHMMSS(segundos_que_quedan) + ' hasta llenado')
+      } else {
+        $('#tiempoHastaLlenado').html('Lleno')
+      }
+    }, 500)
+  }
 }
 
 switchLocation(window.location.pathname.split('/').splice(1))
